ead3042906b65bf3e32aa2c02899a03a7eeb0e7d
Bryan Stitt <bryan@stitthappens.com>
2014-02-26 21:08:13 -0800

    add simple setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import setuptools
+
+
+setuptools.setup(
+    author='Nick W',
+    author_email='falconeer001@gmail.com',
+    description="Implementation of gmaxwell's suggestion of proving account balances",
+    name='proveit',
+    packages=[''],
+    version='0.0.0',
+)
